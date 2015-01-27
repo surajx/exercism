782e8acccf331019417339b6b09ca7c7c356a508
Suraj Narayanan <surajx@gmail.com>
2015-01-27 10:03:02 +0530

    Redability fix
    extracted multiple strip calls to before if-ladder (yes i said strip calls)

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -5,7 +5,8 @@
 #
 def hey(what):
   response_msgs = ['Whatever.', 'Whoa, chill out!', 'Sure.', 'Fine. Be that way!']
+  what = what.strip()
+  if not what: return response_msgs[3] #Silent
   if what.isupper(): return response_msgs[1] #Shouting
-  if not what.strip(): return response_msgs[3] #Silent
-  if what.strip().endswith('?'): return response_msgs[2] #Question
+  if what.endswith('?'): return response_msgs[2] #Question
   return response_msgs[0] #indifference
