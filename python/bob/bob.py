d58cc470712cde2fb86e0e53e8b3d09dbe649dab
Suraj Narayanan <surajx@gmail.com>
2015-01-27 09:49:48 +0530

    bug fix
    Overlooked trailing whitespaces

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -7,5 +7,5 @@ def hey(what):
   response_msgs = ['Whatever.', 'Whoa, chill out!', 'Sure.', 'Fine. Be that way!']
   if what.isupper(): return response_msgs[1] #Shouting
   if not what.strip(): return response_msgs[3] #Silent
-  if what.endswith('?'): return response_msgs[2] #Question
+  if what.strip().endswith('?'): return response_msgs[2] #Question
   return response_msgs[0] #indifference
