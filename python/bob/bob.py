cffca8163fb74b9f3f7e1b72a7e65ca5f2bf32da
Suraj Narayanan <surajx@gmail.com>
2015-01-27 09:48:08 +0530

    Readability change
    Changed negative slice to endswith built-in fn to improve readability

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -7,6 +7,5 @@ def hey(what):
   response_msgs = ['Whatever.', 'Whoa, chill out!', 'Sure.', 'Fine. Be that way!']
   if what.isupper(): return response_msgs[1] #Shouting
   if not what.strip(): return response_msgs[3] #Silent
-  if what.strip()[-1]=='?': return response_msgs[2] #Question
+  if what.endswith('?'): return response_msgs[2] #Question
   return response_msgs[0] #indifference
-
