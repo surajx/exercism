dcf0835a164da409825dabcb75c623c864349647
Suraj Narayanan <surajx@gmail.com>
2015-02-06 01:14:45 +0530

    Using special boolean property to do arithmetic

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,5 +1,5 @@
 def distance(dna_strand1, dna_strand2):
   hamming_distance=0
   for nucleotide1, nucleotide2 in zip(dna_strand1, dna_strand2):
-    if nucleotide1!=nucleotide2: hamming_distance+=1
+    hamming_distance+=nucleotide1!=nucleotide2
   return hamming_distance
